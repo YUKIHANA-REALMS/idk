2d945ebd72dafb2f494b14b9a6fb98291c2f9ab5
YUKIHANA-REALMS <aryandw2010@gmail.com>
2026-06-18 17:16:33 +0530

    Remove Marketplace from plugins and themes pages

---
+++ fix_branding.sql
@@ -0,0 +1,6 @@
+UPDATE setting_option SET setting_value = 'Indium Panel' WHERE setting_key = 'site_title';
+UPDATE setting_option SET setting_value = 'Indium Panel' WHERE setting_key = 'site_name';
+UPDATE setting_option SET setting_value = '' WHERE setting_key = 'site_logo';
+UPDATE setting_option SET setting_value = '' WHERE setting_key = 'site_landing_logo';
+UPDATE setting_option SET setting_value = '' WHERE setting_key = 'site_email_logo';
+UPDATE setting_option SET setting_value = '' WHERE setting_key = 'site_favicon';
